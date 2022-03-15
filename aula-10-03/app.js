461bf0c709591ffbfb84e3d8133a4f0d24400b3c
mateusn3gativ3 <mateusalencar01@gmail.com>
2022-03-15 20:11:33 -0300

    Adicionando site Lovt ao diretorio

--- aula-10-03/app.js
+++ aula-10-03/app.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/cadastro', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/cadastro.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.use(function(req, res, next) {
+    res.status(404).send("desculpe, essa página não existe");
+});
 
 app.listen(3000)
